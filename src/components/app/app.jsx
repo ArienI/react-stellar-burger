df26308368f6c1a66b9a39863800633ba2c2a061
Arien <Laurilin2022@gmail.com>
2023-12-10 20:51:19 +0100

    =^.^= meow =^.^=

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,14 @@
-import styles from "./app.module.css";
-import { data } from "../../utils/data";
+import styles from './App.module.css';
+import { data } from '../../utils/data';
 
 function App() {
   return (
     <div className={styles.app}>
       <pre style={{
-        margin: "auto",
-        fontSize: "1.5rem"
+        margin: 'auto',
+        fontSize: '1.5rem'
       }}>
-        Измените src/components/app/app.jsx и сохраните для обновления....=meow=....
+        Измените src/components/App/App.jsx и сохраните для обновления ... =^.^= meow =^.^= ...
       </pre>
     </div>
   );
