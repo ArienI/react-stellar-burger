f7f22f3733e07426f0bbee567285b78707634c69
Arien <Laurilin2022@gmail.com>
2023-12-21 12:44:04 +0100

    feat: scroll

---
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './BurgerConstructor.module.css';
+import { ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { data } from '../../utils/data';
+
+const BurgerConstructor = () => {
+  const buns = data.filter((item) => item.type === 'bun');
+  // Предполагаем, что первый элемент - это верхняя булка
+  const topBun = buns[0];
+  // Последний элемент - это нижняя булка
+  const bottomBun = buns[buns.length - 1];
+
+  // Отфильтруй остальные ингредиенты
+  const otherIngredients = data.filter((item) => item.type !== 'bun');
+
+
+  return (
+    <section className={styles.burgerConstructor}>
+      {/* Отображение верхней булки */}
+      {topBun && (
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text={`${topBun.name} (верх)`}
+          price={topBun.price}
+          thumbnail={topBun.image}
+          key={topBun._id}
+        />
+      )}
+
+      {/* Отображение ингредиентов с прокруткой */}
+      <div className={styles.customScroll}>
+        {/* Отображаем только остальные ингредиенты */}
+        {otherIngredients.map((item) => (
+          <ConstructorElement
+            key={item._id}
+            text={item.name}
+            price={item.price}
+            thumbnail={item.image}
+          />
+        ))}
+      </div>
+
+      {/* Отображение нижней булки */}
+      {bottomBun && (
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text={`${bottomBun.name} (низ)`}
+          price={bottomBun.price}
+          thumbnail={bottomBun.image}
+          key={bottomBun._id}
+        />
+      )}
+
+      <div className={styles.buttonContainer}>
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default BurgerConstructor;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import MenuItem from './MenuItem';
+import MenuItem from '../MenuItem/MenuItem';
 
 function Header() {
   return (
