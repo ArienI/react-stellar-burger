5ad121137465e220ad7c2f891859f57f87986bb4
Arien <Laurilin2022@gmail.com>
2024-02-21 13:15:25 -0500

    Исправления по ревью

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,20 +1,20 @@
-export const API_URL: string = 'https://norma.nomoreparties.space/api';
+export const API_URL = 'https://norma.nomoreparties.space/api';
 // ingredients actions
-export const SET_INGREDIENTS: string = 'SET_INGREDIENTS';
-export const INCREMENT_AMOUNT: string = 'INCREMENT_AMOUNT';
-export const DECREMENT_AMOUNT: string = 'DECREMENT_AMOUNT';
+export const SET_INGREDIENTS = 'SET_INGREDIENTS';
+export const INCREMENT_AMOUNT = 'INCREMENT_AMOUNT';
+export const DECREMENT_AMOUNT = 'DECREMENT_AMOUNT';
 // burger actions
-export const ADD_INGREDIENT: string = 'ADD_INGREDIENT';
-export const DELETE_INGREDIENT: string = 'DELETE_INGREDIENT';
-export const MOVE_INGREDIENT: string = 'MOVE_INGREDIENT';
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const MOVE_INGREDIENT = 'MOVE_INGREDIENT';
 // order actions
-export const POST_ORDER: string = 'POST_ORDER';
+export const POST_ORDER = 'POST_ORDER';
 // authentication actions
-export const ACTION_TYPE_LOGIN: string = 'ACTION_TYPE_LOGIN';
-export const ACTION_TYPE_LOGOUT: string = 'ACTION_TYPE_LOGOUT';
-export const ACTION_TYPE_SET_USER_DATA: string = 'ACTION_TYPE_SET_USER_DATA';
-export const ACTION_TYPE_SET_USER_LOGGED_IN: string = 'ACTION_TYPE_SET_USER_LOGGED_IN';
-export const ACTION_TYPE_SET_PASSWORD_RESET_CODE_SENT: string = 'ACTION_TYPE_SET_PASSWORD_RESET_CODE_SENT'
-export const ACTION_TYPE_SET_PASSWORD_RESET: string = 'ACTION_TYPE_SET_PASSWORD_RESET'
+export const ACTION_TYPE_LOGIN = 'ACTION_TYPE_LOGIN';
+export const ACTION_TYPE_LOGOUT = 'ACTION_TYPE_LOGOUT';
+export const ACTION_TYPE_SET_USER_DATA = 'ACTION_TYPE_SET_USER_DATA';
+export const ACTION_TYPE_SET_USER_LOGGED_IN = 'ACTION_TYPE_SET_USER_LOGGED_IN';
+export const ACTION_TYPE_SET_PASSWORD_RESET_CODE_SENT = 'ACTION_TYPE_SET_PASSWORD_RESET_CODE_SENT'
+export const ACTION_TYPE_SET_PASSWORD_RESET = 'ACTION_TYPE_SET_PASSWORD_RESET'
 // other
-export const ACCESS_TOKEN_EXPIRATION_TIME_IN_MIN: number = 15;
+export const ACCESS_TOKEN_EXPIRATION_TIME_IN_MIN = 15;
